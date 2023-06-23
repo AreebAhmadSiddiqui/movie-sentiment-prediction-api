24391cfc00074ff3efdec7ebb4dacab512aa1a64
Areeb Ahmad Siddiqui <87814983+AreebAhmadSiddiqui@users.noreply.github.com>
2023-06-23 18:53:24 +0530

    Update app.py manual stop_words

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request, jsonify
 import traceback
 import pickle
 import re
-from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
 from flask_cors import CORS
+
+stop_words=['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 
+'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
 model1 = pickle.load(open('./models/MultinomialNB.pkl', 'rb'))
 model2 = pickle.load(open('./models/Bernoulli.pkl', 'rb'))
 model3 = pickle.load(open('./models/Gaussian.pkl', 'rb'))
@@ -17,7 +20,7 @@ def data_cleaning(text):
     text = re.sub('[^a-zA-Z]', ' ', text)
     text = text.lower()
     text = text.split()
-    text = [ps.stem(word) for word in text if not word in stopwords.words('english')]
+    text = [ps.stem(word) for word in text if not word in stop_words]
     text = ' '.join(text)
     corpus.append(text)
     return countVectorizer(corpus)
